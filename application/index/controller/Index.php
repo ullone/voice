acb48b532d3e4fd68a304f27ac1e015b845ac137
ullone <ullone@163.com>
2017-09-18 17:01:33 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,11 +3,25 @@ namespace app\index\controller;
 
 use \think\View;
 
-class Index
-{
-    public function index()
-    {
-        $view = new \think\View();
-        return $view->fetch();
+class Index {
+    public function index() {
+        $this->checkToken();
+        // $view = new \think\View();
+        // return $view->fetch();
+    }
+    
+    private function checkToken() {
+        $signature = $_GET['signature'];
+        $timestamp = $_GET['timestamp'];
+        $rand      = $_GET['rand'];
+        $token     = '3bfe75bfb2d4752e';
+        $tmpArr = array($token, $timestamp, $rand);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        if($tmpStr == $signature) {
+          echo sha1($token);
+          exit;
+        }
     }
 }
