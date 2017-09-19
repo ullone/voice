d8660d2bed84089693cab174ce17a6c7966be1a4
ullone <ullone@163.com>
2017-09-19 11:52:49 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,19 +8,25 @@ class Index {
         // $this->checkToken();
         // $view = new \think\View();
         // return $view->fetch();
-        if($this->getIsPostRequest()) {
-          $input=file_get_contents("php://input");
-          file_put_contents("log.txt", date('H:i:s')." ".$input."\r\n",FILE_APPEND);
-        } else {
+        // if($this->getIsPostRequest()) {
+        //   $input=file_get_contents("php://input");
+        //   file_put_contents("log.txt", date('H:i:s')." ".$input."\r\n",FILE_APPEND);
+        // } else {
+          $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+          $txt = "Bill Gates\n";
+          fwrite($myfile, $txt);
+          $txt = "Steve Jobs\n";
+          fwrite($myfile, $txt);
+          fclose($myfile);
         //回传后处理 token，token 在官网上有
-          echo sha1("3cb58146e71d1557");
-          file_put_contents("log.txt",date('H:i:s'),FILE_APPEND);
+          // echo sha1("3cb58146e71d1557");
+          // file_put_contents("log.txt",date('H:i:s'),FILE_APPEND);
           // foreach ($_GET as $key=>$value)
           // {
           //   file_put_contents("log.txt", date('H:i:s')." "."_GET: Key: $key; Value: $value"."\r\n", FILE_APPEND);
           // }
-          exit;
-        }
+          // exit;
+
     }
     public function getIsPostRequest()
     {
