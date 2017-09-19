a4e0b725d86a1f57894d4bc634ad3aedf2578500
ullone <ullone@163.com>
2017-09-19 06:22:56 +0000

    fix

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$token = sha1("6a37c34cd75ea12c");
+echo $token;
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+fwrite($myfile, $token.'\n');
+$txt = "Steve Jobs\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+exit();
+?>
