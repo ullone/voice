7abbc2ad51def9fd67a9a7c358b46aa3a9ab72d6
ullone <ullone@163.com>
2017-09-19 16:16:08 +0800

    fix

--- public/index.php
+++ public/index.php
@@ -10,14 +10,7 @@
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
-$token = sha1("6a37c34cd75ea12c");
-echo $token;
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $token.'\n');
-$txt = "Steve Jobs\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-exit();
+
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
