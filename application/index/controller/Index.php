f06cf4bf916587012c73001dbfa97961be1ee34d
ullone <ullone@163.com>
2017-09-21 16:22:09 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -39,7 +39,7 @@ class Index {
         "X-Appid:59c37565",
         "X-CurTime:".$data['timestamp'],
         "X-Param:".$data['param'],
-        "X-CheckSum:".$data['checkSum'],
+        // "X-CheckSum:".$data['checkSum'],
       ];
     	$ch = curl_init();
     	curl_setopt($ch, CURLOPT_URL, $url);
