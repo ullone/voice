91d484fc3b4b95d55a36c8510150502ede1a35c7
ullone <ullone@163.com>
2017-09-21 08:04:17 +0000

    fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //服务器校验
-$token = sha1("6a37c34cd75ea12c");
+$token = sha1("c6261995f22ab4c7");
 echo $token;
 $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 fwrite($myfile, $token.'\n');
