56c075c75b92ed47c1e38e0023468608d12ffd42
ullone <ullone@163.com>
2017-09-21 16:31:00 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,9 +10,9 @@ class Index {
     }
 
     public function semanticComprehension() {
-      // $text   = empty($_POST['text'])?'今天星期几':$_POST['text'];
-      // $text   = base64_encode($text);
-      $text      = 'text=5LuK5aSp5pif5pyf5Yeg';
+      $text   = empty($_POST['text'])?'今天星期几':$_POST['text'];
+      $text   = base64_encode($text);
+      // $text      = 'text=5LuK5aSp5pif5pyf5Yeg';
       $timestamp = time();
       //生成param参数
       $userid = '';
