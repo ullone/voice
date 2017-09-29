8b2fab50ee173891d02f1c10b5c713055b498318
ullone <ullone@163.com>
2017-09-29 14:49:38 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,7 +10,7 @@ class Index {
     }
 
     public function semanticComprehension() {
-      header("Content-type: text/html; charset=utf-8");
+      header("Content-type: text/html; charset=gb2312");
       $text   = empty($_POST['text'])?'今天星期几':$_POST['text'];
       $text   = base64_encode($text);
       $text      = 'text='.$text;
